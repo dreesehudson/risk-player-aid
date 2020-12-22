1efc48ab83c42e72a638fa6147aaeb2485f9eea6
AustinC9 <austin.carson4@gmail.com>
2020-12-22 12:49:54 -0500

    test commit

---
+++ src/Components/Footer.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import { Button } from 'reactstrap';
+
+const Footer = () => {
+  
+    return (
+
+<Button>Austin's Button</Button>
+    )
+}
+
+
+export default Footer;
