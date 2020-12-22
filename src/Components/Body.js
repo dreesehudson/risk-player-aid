bfba570d7adfd71dc2b354b503f112dc40bb2b53
Rmke223 <70180189+Rmke223@users.noreply.github.com>
2020-12-22 12:51:29 -0500

    ryan was here

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -8,7 +8,7 @@ const Example = (props) => {
         <Card body>
           <CardTitle tag="h5">Special Title Treatment</CardTitle>
           <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
-          <Button>Go somewhere</Button>
+          <Button>Ryan was here</Button>
         </Card>
       </Col>
       <Col sm="6">
