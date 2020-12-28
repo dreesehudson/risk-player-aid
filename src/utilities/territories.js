8c917d6e6516b7b5fa4ec5b9eda3c4e6e6939c72
dreesehudson <37116267+dreesehudson@users.noreply.github.com>
2020-12-28 09:36:15 -0500

    Revert "Revert "Reese""

---
+++ src/utilities/territories.js
@@ -0,0 +1,292 @@
+
+export const territories =
+    [
+        {
+            bonus: 3,
+            continent: "Africa",
+            territories: [
+                {
+                    name: 'Central Africa',
+                    owner: null,
+                    minorCity: null,
+                    majorCity: null
+                },
+                {
+                    name: 'East Africa',
+                    owner: null,
+                    minorCity: null,
+                    majorCity: null
+                },
+                {
+                    name: 'Egypt',
+                    owner: null,
+                    minorCity: null,
+                    majorCity: null
+                },
+                {
+                    name: 'North Africa',
+                    owner: null,
+                    minorCity: null,
+                    majorCity: null
+                },
+                {
+                    name: 'Madagascar',
+                    owner: null,
+                    minorCity: null,
+                    majorCity: null
+                },
+                {
+                    name: 'South Africa',
+                    owner: null,
+                    minorCity: null,
+                    majorCity: null
+                },
+            ],
+        },
+        {
+            bonus: 7,
+            continent: "Asia",
+            territories: [
+                {
+                    name: 'Afganistan',
+                    owner: null,
+                    minorCity: null,
+                    majorCity: null
+                },
+                {
+                    name: 'China',
+                    owner: null,
+                    minorCity: null,
+                    majorCity: null
+                },
+                {
+                    name: 'India',
+                    owner: null,
+                    minorCity: null,
+                    majorCity: null
+                },
+                {
+                    name: 'Irkutsk',
+                    owner: null,
+                    minorCity: null,
+                    majorCity: null
+                },
+                {
+                    name: 'Japan',
+                    owner: null,
+                    minorCity: null,
+                    majorCity: null
+                },
+                {
+                    name: 'Kamchatka',
+                    owner: null,
+                    minorCity: null,
+                    majorCity: null
+                },
+                {
+                    name: 'Middle East',
+                    owner: null,
+                    minorCity: null,
+                    majorCity: null
+                },
+                {
+                    name: 'Mongolia',
+                    owner: null,
+                    minorCity: null,
+                    majorCity: null
+                },
+                {
+                    name: 'Siberia',
+                    owner: null,
+                    minorCity: null,
+                    majorCity: null
+                },
+                {
+                    name: 'Southeast Asia',
+                    owner: null,
+                    minorCity: null,
+                    majorCity: null
+                },
+                {
+                    name: 'Ural',
+                    owner: null,
+                    minorCity: null,
+                    majorCity: null
+                },
+                {
+                    name: 'Yakutsk',
+                    owner: null,
+                    minorCity: null,
+                    majorCity: null
+                },
+            ],
+        },
+        {
+            bonus: 2,
+            continent: "Australia",
+            territories: [
+                {
+                    name: 'Eastern Australia',
+                    owner: null,
+                    minorCity: null,
+                    majorCity: null
+                },
+                {
+                    name: 'Indonesia',
+                    owner: null,
+                    minorCity: null,
+                    majorCity: null
+                },
+                {
+                    name: 'New Guinea',
+                    owner: null,
+                    minorCity: null,
+                    majorCity: null
+                },
+                {
+                    name: 'Western Australia',
+                    owner: null,
+                    minorCity: null,
+                    majorCity: null
+                },
+            ],
+        },
+        {
+            bonus: 5,
+            continent: "Europe",
+            territories: [
+                {
+                    name: 'Great Britain',
+                    owner: null,
+                    minorCity: null,
+                    majorCity: null
+                },
+                {
+                    name: 'Iceland',
+                    owner: null,
+                    minorCity: null,
+                    majorCity: null
+                },
+                {
+                    name: 'Scandinavia',
+                    owner: null,
+                    minorCity: null,
+                    majorCity: null
+                },
+                {
+                    name: 'Russia',
+                    owner: null,
+                    minorCity: null,
+                    majorCity: null
+                },
+                {
+                    name: 'Northern Europe',
+                    owner: null,
+                    minorCity: null,
+                    majorCity: null
+                },
+                {
+                    name: 'Western Europe',
+                    owner: null,
+                    minorCity: null,
+                    majorCity: null
+                },
+                {
+                    name: 'Southern Europe',
+                    owner: null,
+                    minorCity: null,
+                    majorCity: null
+                },
+            ],
+        },
+        {
+            bonus: 5,
+            continent: "North America",
+            territories: [
+                {
+                    name: 'Alaska',
+                    owner: null,
+                    minorCity: null,
+                    majorCity: null
+                },
+                {
+                    name: 'Alberta',
+                    owner: null,
+                    minorCity: null,
+                    majorCity: null
+                },
+                {
+                    name: 'Central America',
+                    owner: null,
+                    minorCity: null,
+                    majorCity: null
+                },
+                {
+                    name: 'Eastern Canada',
+                    owner: null,
+                    minorCity: null,
+                    majorCity: null
+                },
+                {
+                    name: 'Eastern US',
+                    owner: null,
+                    minorCity: null,
+                    majorCity: null
+                },
+                {
+                    name: 'Greenland',
+                    owner: null,
+                    minorCity: null,
+                    majorCity: null
+                },
+                {
+                    name: 'Ontario',
+                    owner: null,
+                    minorCity: null,
+                    majorCity: null
+                },
+                {
+                    name: 'Northwest Territory',
+                    owner: null,
+                    minorCity: null,
+                    majorCity: null
+                },
+                {
+                    name: 'Western US',
+                    owner: null,
+                    minorCity: null,
+                    majorCity: null
+                },
+            ],
+        },
+        {
+            bonus: 2,
+            continent: "South America",
+            territories: [
+                {
+                    name: 'Argentina',
+                    owner: null,
+                    minorCity: null,
+                    majorCity: null
+                },
+                {
+                    name: 'Brazil',
+                    owner: null,
+                    minorCity: null,
+                    majorCity: null
+                },
+                {
+                    name: 'Peru',
+                    owner: null,
+                    minorCity: null,
+                    majorCity: null
+                },
+                {
+                    name: 'Venezuela',
+                    owner: null,
+                    minorCity: null,
+                    majorCity: null
+                },
+            ],
+        },
+    ]
