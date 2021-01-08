0daeb997c4d40207e8c75c65a23cfd23484edd2f
Reese Hudson <david.reese.hudson@gmail.com>
2021-01-08 14:46:56 -0500

    add odds bar to combat modal

--- src/Components/PlayerDashboard.js
+++ src/Components/PlayerDashboard.js
@@ -7,8 +7,10 @@ import RedStarTracker from './RedStarTracker'
 import Recruitment from './Recruitment'
 import { worldMap } from '../utilities/territories'
 import Select from "react-select";
+import { Progress } from 'reactstrap';
 
 import MapTabs from './MapTabs';
+import Odds from './Odds';
 
 const Body = (props) => {
     const [battleground, setBattleground] = useState('');
@@ -45,7 +47,7 @@ const Body = (props) => {
                     <Card body>
                         <CardTitle tag="h5">Territories</CardTitle>
 
-                        <MapTabs/>
+                        <MapTabs />
 
 
                     </Card>
@@ -68,67 +70,70 @@ const Body = (props) => {
                                     <Modal isOpen={modal} size='lg' toggle={toggle} className={className}>
                                         <ModalHeader toggle={toggle}></ModalHeader>
                                         <ModalBody>
-                                            <Row>
-                                                <Col className="justify-content-center">
-                                                    <Label>Battleground</Label>
-                                                    <Select
-                                                        className="text-dark"
-                                                        defaultValue={'Select...'}
-                                                        onChange={setBattleground}
-                                                        options={worldMap}
-                                                    />
-                                                </Col>
-                                                <Col className="justify-content-center">
-                                                    <Label>Attacking From</Label>
-                                                    {battleground ?
+                                            <>
+                                                <Odds />
+                                                <Row>
+                                                    <Col className="justify-content-center">
+                                                        <Label>Battleground</Label>
                                                         <Select
                                                             className="text-dark"
-                                                            defaultValue={'Select Battleground First'}
-                                                            onChange={setAttackingFrom}
-                                                            //needs to only display the adjacent territories to the selected battleground (part of the territory object)
+                                                            defaultValue={'Select...'}
+                                                            onChange={setBattleground}
                                                             options={worldMap}
                                                         />
-                                                        :
-                                                        <Select
-                                                            disabled
-                                                            className="text-dark"
-                                                            defaultValue={'Pick Battleground'}
-                                                            onChange={setAttackingFrom}
-                                                            //needs to only display the adjacent territories to the selected battleground (part of the territory object)
-                                                            options={worldMap}
-                                                        />
+                                                    </Col>
+                                                    <Col className="justify-content-center">
+                                                        <Label>Attacking From</Label>
+                                                        {battleground ?
+                                                            <Select
+                                                                className="text-dark"
+                                                                defaultValue={'Select Battleground First'}
+                                                                onChange={setAttackingFrom}
+                                                                //needs to only display the adjacent territories to the selected battleground (part of the territory object)
+                                                                options={worldMap}
+                                                            />
+                                                            :
+                                                            <Select
+                                                                disabled
+                                                                className="text-dark"
+                                                                defaultValue={'Pick Battleground'}
+                                                                onChange={setAttackingFrom}
+                                                                //needs to only display the adjacent territories to the selected battleground (part of the territory object)
+                                                                options={worldMap}
+                                                            />
 
-                                                    }
-                                                </Col>
-                                            </Row>
-                                            <Row>
-                                                <Col className="justify-content-center">
-                                                    <Label>Defender</Label>
-                                                    <Input></Input>
-                                                </Col>
-                                                <Col className="justify-content-center">
-                                                    <Label>Attacker</Label>
-                                                    <Input></Input>
-                                                </Col>
-                                            </Row>
-                                            <Row>
-                                                <Col className='justify-content-center col-6'>
-                                                    {
-                                                        battleground.bunker ?
-                                                            <FontAwesomeIcon className='m-2 mt-3 fa-3x' color={'#F18F01'} icon={faFortAwesomeAlt} />
-                                                            :
-                                                            <FontAwesomeIcon className='m-2 mt-3 fa-3x' icon={faFortAwesomeAlt} />
-                                                    }
-                                                    {
-                                                        battleground.ammoShortage ?
-                                                            <FontAwesomeIcon className='m-2 mt-3 fa-3x' color={'red'} icon={faBan} />
-                                                            :
-                                                            <FontAwesomeIcon className='m-2 mt-3 fa-3x' icon={faBan} />
-                                                    }
-                                                </Col>
-                                                <Col className='col-6'>
-                                                </Col>
-                                            </Row>
+                                                        }
+                                                    </Col>
+                                                </Row>
+                                                <Row>
+                                                    <Col className="justify-content-center">
+                                                        <Label>Defender</Label>
+                                                        <Input></Input>
+                                                    </Col>
+                                                    <Col className="justify-content-center">
+                                                        <Label>Attacker</Label>
+                                                        <Input></Input>
+                                                    </Col>
+                                                </Row>
+                                                <Row>
+                                                    <Col className='justify-content-center col-6'>
+                                                        {
+                                                            battleground.bunker ?
+                                                                <FontAwesomeIcon className='m-2 mt-3 fa-3x' color={'#F18F01'} icon={faFortAwesomeAlt} />
+                                                                :
+                                                                <FontAwesomeIcon className='m-2 mt-3 fa-3x' icon={faFortAwesomeAlt} />
+                                                        }
+                                                        {
+                                                            battleground.ammoShortage ?
+                                                                <FontAwesomeIcon className='m-2 mt-3 fa-3x' color={'red'} icon={faBan} />
+                                                                :
+                                                                <FontAwesomeIcon className='m-2 mt-3 fa-3x' icon={faBan} />
+                                                        }
+                                                    </Col>
+                                                    <Col className='col-6'>
+                                                    </Col>
+                                                </Row>
+                                            </>
                                         </ModalBody>
                                         <ModalFooter className="justify-content-center">
                                             <Button color="primary" onClick={toggle}>Roll</Button>{' '}
