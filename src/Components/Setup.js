560a95ef76555c42568e6278b6e7764ed1efea9f
Reese Hudson <david.reese.hudson@gmail.com>
2021-01-12 16:07:57 -0500

    selecting 3-5 players provides correct number of name input fields

--- src/Components/Setup.js
+++ src/Components/Setup.js
@@ -6,10 +6,23 @@ import playerContext from '../utilities/PlayerContext';
 const Setup = (props) => {
     const [draft, setDraft] = useState(false)
 
-    const { players, addPlayer, removePlayer } = useContext(playerContext)
+    const { players, setPlayers } = useContext(playerContext)
+    const [numberOfPlayers, setNumberOfPlayers] = useState(3)
+    const [playersLocked, setPlayersLocked] = useState(false)
 
-
-
+    function addPlayer() {
+        setNumberOfPlayers(numberOfPlayers + 1)
+        let proxy = players
+        proxy.push({ id: players.length, name: "", faction: {} })
+        setPlayers(proxy)
+    }
+    
+    function removePlayer() {
+        setNumberOfPlayers(numberOfPlayers - 1)
+        let proxy = players
+        proxy.pop()
+        setPlayers(proxy)
+    }
 
     return (
         <Container className='mt-3'>
@@ -17,75 +30,87 @@ const Setup = (props) => {
             <Row className='justify-content-center my-3'>
                 <h3 className='mr-2'>Number of Players:</h3>
                 {
-                    players.length > 3 ?
+                    numberOfPlayers > 3 ?
                         <Button onClick={() => removePlayer()} className='mx-2 p-2'>-</Button>
                         :
                         <Button disabled className='mx-2 p-2'>-</Button>
                 }
-                <h3 className='my-1'>{players.length}</h3>
+                <h3 className='my-1'>{numberOfPlayers}</h3>
                 {
-                    players.length < 5 ?
-                        <Button onClick={() => addPlayer()} className='mx-2 p-2'>+</Button>
+                    numberOfPlayers < 5 ?
+                        <Button onClick={() => addPlayer(numberOfPlayers + 1)} className='mx-2 p-2'>+</Button>
                         :
                         <Button disabled className='mx-2 p-2'>+</Button>
                 }
-
             </Row>
             {/* Names of Players */}
             <Row>
                 {
-                    <Col className='col-3'>
-                        <Label>Name:</Label>
-                        <Input />
-                    </Col>
-                }
-            </Row>
-            {
-                draft ?
-                    //show draft cards and snake draft for Faction, Turn Order, HQ Placement Order, Starting Troops, Coin Cards
-                    <>
-                        <h3>Draft True</h3>
-                    </>
-                    :
-                    <>
-                        <h3>Draft False</h3>
-                    </>
-                //Pick Order to select Faction, Turn Order, HQ Placement Order
-            }
-            <Row>
-                {
-                    factions.map((faction, idx) => {
+                    players.map((player, key) => {
                         return (
-                            <>
-                                {
-                                    faction.unlocked &&
-                                    <Col className='col-6 mb-3' >
-                                        <Card>
-                                            <CardHeader>{faction.name}</CardHeader>
-                                            <CardBody>
-                                                <ul>
-                                                    {faction.bringerOfNuclearFire && <li className='mb-2'>Bringer of Nuclear Fire</li>}
-                                                    {faction.startingPower && <li className='mb-2'>{faction.startingPower[0]}</li>}
-                                                    {faction.startingPower && <li className='mb-2'>{faction.startingPower[1]}</li>}
-                                                    {faction.comebackPower && <li className='mb-2'>{faction.comebackPower}</li>}
-                                                    {faction.misslePower && <li className='mb-2'>{faction.misslePower}</li>}
-                                                    {faction.weakness && <li className='mb-2'>{faction.weakness}</li>}
-                                                    {faction.privateMission && <li className='mb-2'>{faction.privateMission}</li>}
-                                                </ul>
-                                            </CardBody>
-                                        </Card>
-                                    </Col>
-                                }
-                            </>
+                            <Col key={key}>
+                                <Label>Player Name</Label>
+                                <Input required/>
+                            </Col>
                         )
                     })
                 }
             </Row>
-            <Row>
-                {
+            {
+                playersLocked ?
+                    <>
+                        {
+                            draft ?
+                                //show draft cards and snake draft for Faction, Turn Order, HQ Placement Order, Starting Troops, Coin Cards
+                                <>
+                                    <h3>Draft True</h3>
+                                </>
+                                :
+                                <>
+                                    <h3>Draft False</h3>
+                                </>
+                            //Pick Order to select Faction, Turn Order, HQ Placement Order
+                        }
+                        <Row>
+                            {
+                                factions.map((faction, idx) => {
+                                    return (
+                                        <>
+                                            {
+                                                faction.unlocked &&
+                                                <Col className='col-6 mb-3' >
+                                                    <Card>
+                                                        <CardHeader>{faction.name}</CardHeader>
+                                                        <CardBody>
+                                                            <ul>
+                                                                {faction.bringerOfNuclearFire && <li className='mb-2'>Bringer of Nuclear Fire</li>}
+                                                                {faction.startingPower && <li className='mb-2'>{faction.startingPower[0]}</li>}
+                                                                {faction.startingPower && <li className='mb-2'>{faction.startingPower[1]}</li>}
+                                                                {faction.comebackPower && <li className='mb-2'>{faction.comebackPower}</li>}
+                                                                {faction.misslePower && <li className='mb-2'>{faction.misslePower}</li>}
+                                                                {faction.weakness && <li className='mb-2'>{faction.weakness}</li>}
+                                                                {faction.privateMission && <li className='mb-2'>{faction.privateMission}</li>}
+                                                            </ul>
+                                                        </CardBody>
+                                                    </Card>
+                                                </Col>
+                                            }
+                                        </>
+                                    )
+                                })
+                            }
+                        </Row>
+                        <Row>
+                            {
 
-                }
-            </Row>
+                            }
+                        </Row>
+                    </>
+                    :
+                    <>
+                    <Button className='my-3' onClick={() => setPlayersLocked(true)}>Submit</Button>
+                    </>
+            }
 
         </Container >
     );
