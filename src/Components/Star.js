ebffcb7e262f171bd0a8f5ea41f045360638b245
Reese Hudson <david.reese.hudson@gmail.com>
2021-01-11 22:38:19 -0500

    star is toggleable

--- src/Components/Star.js
+++ src/Components/Star.js
@@ -4,15 +4,16 @@ import { faStar } from '@fortawesome/free-solid-svg-icons'
 
 export default function RedStarTracker() {
     const [color, setColor] = useState()
+    const [clicked, setClicked] = useState(false)
 
-    function changeColor(){
-        //take current color
-        
-        //change to opposite color
-
-    }
-    
     return (
-        <FontAwesomeIcon className='mx-2 fa-2x' color={color} onClick={() => changeColor} icon={faStar} />
+        <>
+            {
+                clicked ?
+                    <FontAwesomeIcon className='mx-2 fa-2x' color='red' onClick={() => setClicked(!clicked)} icon={faStar} />
+                    :
+                    <FontAwesomeIcon className='mx-2 fa-2x' onClick={() => setClicked(!clicked)} icon={faStar} />
+            }
+        </>
     )
 }
