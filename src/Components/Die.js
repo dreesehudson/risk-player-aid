6f10f005a2d5ff6aa606e676342e0e835b38a425
Reese Hudson <david.reese.hudson@gmail.com>
2021-01-27 15:12:36 -0500

    issue 5, do a combat roll

---
+++ src/Components/Die.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faDiceOne, faDiceTwo, faDiceThree, faDiceFour, faDiceFive, faDiceSix } from '@fortawesome/free-solid-svg-icons'
+
+const Die = (props) => {
+
+    return (
+        <>
+            {
+                props.die === 1 && 
+                    <FontAwesomeIcon className='m-2 mt-3 fa-2x' icon={faDiceOne} />            
+            }
+            {
+                props.die === 2 && 
+                    <FontAwesomeIcon className='m-2 mt-3 fa-2x' icon={faDiceTwo} />            
+            }
+            {
+                props.die === 3 && 
+                    <FontAwesomeIcon className='m-2 mt-3 fa-2x' icon={faDiceThree} />            
+            }
+            {
+                props.die === 4 && 
+                    <FontAwesomeIcon className='m-2 mt-3 fa-2x' icon={faDiceFour} />            
+            }
+            {
+                props.die === 5 && 
+                    <FontAwesomeIcon className='m-2 mt-3 fa-2x' icon={faDiceFive} />            
+            }
+            {
+                props.die === 6 && 
+                    <FontAwesomeIcon className='m-2 mt-3 fa-2x' icon={faDiceSix} />            
+            }
+        </>
+    )
+}
+
+export default Die
